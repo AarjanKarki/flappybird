3296f7c944310198295665ce4058978c9d924a77
AarjanKarki <aarjankarki2000@gmail.com>
2025-03-22 18:07:20 +0000

    Add files via upload

--- flappybird.py
+++ flappybird.py
@@ -1,13 +1,14 @@
 import pygame, os
 import time
+import random
 WIDTH=864
 HEIGHT=936
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
 #define game variables
 ground_scroll = 0
-scroll_speed = 4
-flying = False
+scroll_speed = 0.5
+flying = True
 game_over = False
 pipe_gap = 150
 pipe_frequency = 1500 #milliseconds
@@ -16,10 +17,18 @@ score = 0
 pass_pipe = False
 floor_x=0
 
+
+#creating a bird group
+birdgroup=pygame.sprite.Group()
+pipegroup=pygame.sprite.Group()
+
 #loading and images
 bg=pygame.image.load(os.path.join('flappyball','flappybirdbg.png'))
 floor=pygame.image.load(os.path.join('flappyball','floor.png'))
 pipe=pygame.image.load(os.path.join('flappyball','pipe.png'))
+
+#checking if the pipe is facing up or down
+pipeup=1
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -35,8 +44,27 @@ class Bird(pygame.sprite.Sprite):
         self.rect.center=[x,y]
         self.vel=0
         self.clicked=False
-flappy=Bird(100,HEIGHT//2)
 
+class Pipes(pygame.sprite.Sprite):
+    def __init__(self,x,y,pipeup):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load('C:/Users/amark/pygame2module/flappyball/pipe.png')
+        self.rect=self.image.get_rect()
+        if pipeup==1:
+            self.image=pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft=[x,y-pipe_gap//2]
+        elif pipeup==-1:
+            self.rect.topleft=[x,y+pipe_gap//2]
+            
+        #elif pipeup==-1:
+    def update(self):
+        self.rect.x-=scroll_speed
+        if self.rect.right<0:
+            self.kill()
+
+
+flappy=Bird(100,HEIGHT//2)
+birdgroup.add(flappy)
 
 
         
@@ -52,13 +80,23 @@ while running:
     #drawing and scrolling the ground
     screen.blit(bg,(0,0))
     screen.blit(floor,(floor_x,767))
-    floor_x-=0.5
-    flappy.draw()
-    if floor_x<-30:
-        floor_x=0
+    birdgroup.draw(screen)
+    pipegroup.draw(screen)
+    birdgroup.update()
+    pipegroup.update()
+    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+        
+    if flying==True and game_over==False:
+        pipeheight=random.randint(0,600)
+        bottompipe=Pipes(WIDTH,HEIGHT//2+pipeheight,1)
+        pipegroup.add(bottompipe) 
+        floor_x-=scroll_speed
+        if floor_x<-30:
+            floor_x=0
+
     pygame.display.update()
 
 
